c0c17d761476722e4056d5c7ccc0d9774142d82e
pbl64k <pbl64k@gmail.com>
2015-05-25 12:21:03 +0300

    Mmm.

--- fact.hs
+++ fact.hs
@@ -1,16 +1,16 @@
 import Data.Functor
 
-data I x = I x
-data K x y = K x
+data I x = I { i :: x }
+data K x y = K { k :: x }
 data Sum x y z = SumL (x z) | SumR (y z)
 data Prod x y z = Prod (x z) (y z)
-data Mu x = In (x (Mu x))
+data Mu x = In { out :: x (Mu x) }
 
 instance Functor I where
-    f `fmap` (I x) = I (f x)
+    fmap f = I . f . i
 
 instance Functor (K x) where
-    f `fmap` (K x) = K x
+    fmap f = K . k
 
 instance (Functor x, Functor y) => Functor (Sum x y) where
     f `fmap` (SumL x) = SumL (f `fmap` x)
@@ -19,54 +19,63 @@ instance (Functor x, Functor y) => Functor (Sum x y) where
 instance (Functor x, Functor y) => Functor (Prod x y) where
     f `fmap` (Prod x y) = Prod (f `fmap` x) (f `fmap` y)
 
-unI (I x) = x
+f `appI` (I x) = f x
 
-unK (K x) = x
+f `appK` (K x) = f x
 
-unSum f _ (SumL x) = f x
-unSum _ g (SumR x) = g x
+(f, _) `appSum` (SumL x) = f x
+(_, g) `appSum` (SumR x) = g x
 
-unProd f (Prod x y) = f x y
+f `appProd` (Prod x y) = f x y
 
-out (In x) = x
+-- Folds and unfolds for "free"!
 
-f `cata` (In x) = f ((f `cata`) `fmap` x)
+cata :: Functor f => (f a -> a) -> Mu f -> a
+cata f = f . ((f `cata`) `fmap`) . out
 
-f `ana` x = In ((f `ana`) `fmap` (f x))
+ana :: Functor f => (a -> f a) -> a -> Mu f
+ana f = In . ((f `ana`) `fmap`) . f
 
 type Nat = Mu (Sum (K ()) I)
 
-showNat :: Nat -> Int
-showNat = cata (unSum (const 0) (succ . unI))
+showNat :: Nat -> Integer
+showNat = (((const 0, (succ `appI`)) `appSum`) `cata`)
 
-readNat :: Int -> Nat
-readNat = ana (\x -> if x == 0 then SumL (K ()) else SumR (I (pred x)))
+readNat :: Integer -> Nat
+readNat = ((\x -> if x == 0 then SumL (K ()) else SumR (I (pred x))) `ana`)
 
 zero :: Nat
 zero = (In . SumL . K) ()
 
+suc :: Nat -> Nat
 suc = In . SumR . I
 
-prd = unSum (const zero) unI . out
+prd :: Nat -> Nat
+prd = ((const zero, (id `appI`)) `appSum`) . out
 
-plus x = cata (unSum (const x) (suc . unI))
+plus :: Nat -> Nat -> Nat
+plus x = (((const x, (suc `appI`)) `appSum`) `cata`)
 
-mult x = cata (unSum (const zero) ((`plus` x) . unI))
+mult :: Nat -> Nat -> Nat
+mult x = (((const zero, ((`plus` x) `appI`)) `appSum`) `cata`)
 
 type List a = Mu (Sum (K ()) (Prod (K a) I))
 
 nil :: List a
 nil = (In . SumL . K) ()
 
-cons = ((In . SumR) .) . (Prod . K)
+cons :: a -> List a -> List a
+cons x = In . SumR . Prod (K x) . I
 
-unf :: Nat -> List Nat
-unf = ana (unSum (const (SumL (K ()))) ((\x -> SumR (Prod (K (suc x)) (I x))) . unI) . out)
+downto1 :: Nat -> List Nat
+downto1 = ((((const (SumL (K ())), ((\x -> SumR (Prod (K (suc x)) (I x))) `appI`)) `appSum`) . out) `ana`)
 
 prod :: List Nat -> Nat
-prod = cata (unSum (const (suc zero)) (unProd (\x y -> (unK x) `mult` (unI y))))
+prod = (((const (suc zero), ((\x y -> (id `appK` x) `mult` (id `appI` y)) `appProd`)) `appSum`) `cata`)
 
-fact = prod . unf
+fact :: Nat -> Nat
+fact = prod . downto1
 
+factorial :: Integer -> Integer
 factorial = showNat . fact . readNat
 
